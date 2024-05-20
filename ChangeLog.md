5b76d854602f223f1b8fb001a46dc98cf3a31b26
amirparag2005 <amirparag2022@mail.ru>
2024-05-20 03:03:00 -0400

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,2 @@
+* Mon May 20 2024 amirparag2005 <amirparag2022@mail.ru> 0.1.0.0
+- Initial RPM release
---
+++ tests/tests.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "main.cpp"
+
+class CatMock : public Cat{}
+class DogMock : public Dog{}
+class BirdMock : public Bird{}
+
+
+//First test
+TEST(CatMock, Mock){
+    CatMock cat("Louis");
+    EXPECT_CALL(cat, name()).Times(1);
+    EXPECT_EQ(cat.name(), "Louis");
+}
+
+//Second test
+TEST(DogMock, Mock){
+    DogMock dog("Frank");
+    EXPECT_CALL(dog, name()).Times(1);
+    EXPECT_EQ(dog.name(), "Frank");
+}
+
+//Third test
+TEST(BirdMock, Mock){
+    BirdMock bird("Adam");
+    EXPECT_CALL(bird, name()).Times(1);
+    EXPECT_EQ(bird.name(), "Adam");
+}
